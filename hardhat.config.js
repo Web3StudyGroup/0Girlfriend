b80fede32a228235c0fb5aed999da205d3e86ead
Web3StudyGroup <hujunjob123@gmail.com>
2025-09-13 17:14:31 +0800

    deploy on localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,10 @@ import "@nomicfoundation/hardhat-toolbox";
 export default {
   solidity: "0.8.24",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337
+    },
     "0g-testnet": {
       url: "https://evmrpc-testnet.0g.ai",
       chainId: 16601,
